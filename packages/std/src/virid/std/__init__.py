d552f916f861138c2353c0690bbbf19a5c351668
Ailrid <ailrid.code@gmail.com>
2026-06-16 20:34:30 +0800

    chore: add gitignore and remove pycache

--- packages/std/src/virid/std/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) 2026-present Ailrid.
-Licensed under the Apache License, Version 2.0.
-Project: Virid
-"""
-
-from .plugin import StdPlugin
-from .utils import execute_group, next_tick, execute_block
